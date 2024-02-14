e4b09bed1390a2a40525e5802f3db7ce6aa1b6ab
uurtech <girisiyorum@gmail.com>
2024-02-14 20:21:08 +0300

    up

--- uurtech.js
+++ uurtech.js
@@ -1,18 +1,23 @@
 function get_elements_by_inner(word) {
     res = []
     elems = [...document.getElementsByTagName('span')];
-    elems.forEach((elem) => { 
-        if(elem.outerHTML.includes(word)) {
+    elems.forEach((elem) => {
+        if (elem.outerHTML.includes(word)) {
             res.push(elem)
         }
     })
-    return(res)
+    return (res)
 }
 
 
-let title = get_elements_by_inner("#1#")[0]
+(function () {
+    console.log('uurtech.js executed');
+    let title = get_elements_by_inner("#1#")[0]
+    // title.textContent = 
+    let answerA = get_elements_by_inner("#2#")[0]
+    let correctAnswer = get_elements_by_inner("#3#")[0]
+    let answerB = get_elements_by_inner("#4#")[0]
 
-let answerA = get_elements_by_inner("#2#")[0]
-let correctAnswer = get_elements_by_inner("#3#")[0]
-let answerB = get_elements_by_inner("#4#")[0]
+})();
+
 
