6bbb966b6cccfe35767050cb2639ff3296040930
uurtech <girisiyorum@gmail.com>
2024-02-14 20:12:42 +0300

    up

--- conveter.js
+++ conveter.js
@@ -7,4 +7,10 @@ const sheetName = workbook.SheetNames[0]; // Assuming the data is in the first s
 const worksheet = workbook.Sheets[sheetName];
 const data = xlsx.utils.sheet_to_json(worksheet);
 
-console.log(data);
+fs.writeFile('./data/sorular.json', JSON.stringify(data), 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+}});
+
+
