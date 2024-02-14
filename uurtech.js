058bd0a43d4ba040efa2a62fc066d9ac01489a79
uurtech <girisiyorum@gmail.com>
2024-02-14 19:54:17 +0300

    up

--- uurtech.js
+++ uurtech.js
@@ -1,13 +1,18 @@
-let data = "What does S3 stand for in AWS?;Secure Storage Solution;Simple Storage Service;Serverless Storage System\t
-Which AWS service is used to deploy and manage containerized applications at scale?;EC2;ECS;S3\t
-AWS service is used to set up and manage a virtual private cloud (VPC)?;EC2;VPC;S3"
+function get_elements_by_inner(word) {
+    res = []
+    elems = [...document.getElementsByTagName('span')];
+    elems.forEach((elem) => { 
+        if(elem.outerHTML.includes(word)) {
+            res.push(elem)
+        }
+    })
+    return(res)
+}
 
 
+let title = get_elements_by_inner("#1#")[0]
 
-let title = document.querySelector("#\\:r0\\:0 > div > div > div > main > div.vWU3Dw > div.NmadZQ > div > div > div.WVSfHg > div > div > div > div.dYa4Xg > div > div > div.xvJQZA > div > div > div > div:nth-child(15) > div > div > div > p > span")
-title.textContent = "hello"
-
-let answerA = document.querySelector("#\\:r0\\:0 > div > div > div > main > div.vWU3Dw > div.NmadZQ > div > div > div.WVSfHg > div > div > div > div.dYa4Xg > div > div > div.xvJQZA > div > div > div > div:nth-child(10) > div > div:nth-child(3) > div > div > div > p > span")
-let correctAnswer = document.querySelector("#\\:r0\\:0 > div > div > div > main > div.vWU3Dw > div.NmadZQ > div > div > div.WVSfHg > div > div > div > div.dYa4Xg > div > div > div.xvJQZA > div > div > div > div:nth-child(11) > div > div:nth-child(3) > div > div > div > p > span")
-let answerB = document.querySelector("#\\:r0\\:0 > div > div > div > main > div.vWU3Dw > div.NmadZQ > div > div > div.WVSfHg > div > div > div > div.dYa4Xg > div > div > div.xvJQZA > div > div > div > div:nth-child(12) > div > div:nth-child(3) > div > div > div > p > span")
+let answerA = get_elements_by_inner("#2#")[0]
+let correctAnswer = get_elements_by_inner("#3#")[0]
+let answerB = get_elements_by_inner("#4#")[0]
 
